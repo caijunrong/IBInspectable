5d4349b18267bcddf461a5b31a83545068494a90
caijunrong <caijunrong@localhost>
2016-03-19 21:49:52 +0800

    Initial Commit

---
+++ TestIBInspectable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestIBInspectable
+//
+//  Created by caijunrong on 16/3/19.
+//  Copyright © 2016年 caijunrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
